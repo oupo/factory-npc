3db3903d098b7549661ee87ab837e7fce1b04a95
oupo <oupo.nejiki@gmail.com>
2014-11-03 16:09:34 +0900

    検索フォーム作り

--- factory-npc.ts
+++ factory-npc.ts
@@ -1,223 +1,129 @@
-﻿function u32(x: number): number {
-    return x >>> 0;
-}
+﻿/// <reference path="jquery.d.ts" />
 
-function mul(a: number, b: number): number {
-    var a1 = a >>> 16, a2 = a & 0xffff;
-    var b1 = b >>> 16, b2 = b & 0xffff;
-    return u32(((a1 * b2 + a2 * b1) << 16) + a2 * b2);
-}
-
-class LCG {
-    seed: number;
-
-    constructor(seed: number) {
-        this.seed = seed;
-    }
-
-    rand(): number {
-        this.seed = u32(mul(this.seed, 0x41c64e6d) + 0x6073);
-        return this.seed >>> 16;
-    }
-}
-
-interface SpendingLog {
-    frame: number;
-    npcNo: number;
-    description: string;
-}
-
-interface ActionLog {
-    frame: number;
-    npcNo: number;
-    direction: Direction;
-    isMove: boolean;
-}
-
-enum NPCType {
-    MOVABLE,
-    NONMOVABLE
-}
-
-enum Direction {
-    UP = 0,
-    DOWN,
-    LEFT,
-    RIGHT
-}
-
-interface NPC {
-    no: number;
-    name: string;
-    type: NPCType;
-    width: number;
-    height: number;
-    allDirections: Array<number>;
-    x: number;
-    y: number;
-    timer: number;
-    direction: Direction;
-    state: number;
-}
-
-var DX = [0, 0, -1, 1];
-var DY = [-1, 1, 0, 0];
-
-function movable(npc: NPC): boolean {
-    var newX = npc.x + DX[npc.direction];
-    var newY = npc.y + DY[npc.direction];
-    return 0 <= newX && newX < npc.width && 0 <= newY && newY < npc.height;
-}
-
-function move(npc: NPC) {
-    npc.x += DX[npc.direction];
-    npc.y += DY[npc.direction];
-}
-
-function movableNpc(no: number, name: string, width: number, height: number, x: number, y: number, direction: Direction): NPC {
-    return {
-        no: no,
-        name: name,
-        type: NPCType.MOVABLE,
-        width: width,
-        height: height,
-        allDirections: [0, 1, 2, 3],
-        x: x,
-        y: y,
-        timer: 0,
-        direction: direction,
-        state: 0
-    };
-}
-
-function nonmovableNpc(no: number, name: string, allDirections: Array<Direction>, direction: Direction): NPC {
-    return {
-        no: no,
-        name: name,
-        type: NPCType.NONMOVABLE,
-        width: 0,
-        height: 0,
-        allDirections: allDirections,
-        x: 0,
-        y: 0,
-        timer: 0,
-        direction: direction,
-        state: 0
-    };
-}
-
-class Context {
-    lcg: LCG;
-    frame: number;
-    npcs: Array<NPC>;
-    spendingLog: Array<SpendingLog>;
-    actionLog: Array<ActionLog>;
-
-    constructor(seed: number, npcs: Array<NPC>) {
-        this.lcg = new LCG(seed);
-        this.frame = 0;
-        this.npcs = npcs;
-        this.spendingLog = [];
-        this.actionLog = [];
-    }
-
-    init() {
-        this.npcs.forEach((npc) => {
-            if (npc.type == NPCType.NONMOVABLE) {
-                npc.timer = this.randTime(npc);
-            }  
-        });
-    }
-
-    advance() {
-        this.npcs.forEach((npc) => {
-            this.advanceNpc(npc);
-        });
-    }
-
-    incFrame(n: number) {
-        this.frame += n;
-    }
-
-    advanceNpc(npc: NPC) {
-        if (npc.type == NPCType.NONMOVABLE) {
-            npc.timer -= 1;
-            if (npc.timer == 0) {
-                npc.timer = this.randTime(npc);
-                var direction = this.randDirection(npc);
-                if (npc.direction != direction) {
-                    this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: direction, isMove: false });
-                }
-                npc.direction = direction;
-            }
-        } else {
-            switch (npc.state) {
-            case 0:
-                npc.state = 1;
-                break;
-            case 1:
-                npc.timer = this.randTime(npc) - 1;
-                npc.state = 2;
-                break;
-            case 2:
-                npc.timer -= 1;
-                if (npc.timer == 0) {
-                    var prevDirection = npc.direction;
-                    npc.direction = this.randDirection(npc);
-                    if (movable(npc)) {
-                        this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: npc.direction, isMove: true });
-                        move(npc);
-                        npc.state = 4;
-                        npc.timer = 7;
-                    } else {
-                        if (npc.direction != prevDirection) {
-                            this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: npc.direction, isMove: false });
-                        }
-                        npc.state = 0;
-                    }
-                }
-                break;
-            case 4:
-                npc.timer -= 1;
-                if (npc.timer == 0) {
-                    npc.state = 0;
-                }
-                break;
-            }
-        }
-    }
-
-    randTime(npc: NPC): number {
-        var time = 16 * (this.lcg.rand() % 4 + 1);
-        this.spendingLog.push({ frame: this.frame, npcNo: npc.no, description: "time -> " + time });
-        return time;
-    }
-
-    randDirection(npc: NPC): number {
-        var direction = npc.allDirections[this.lcg.rand() % npc.allDirections.length];
-        this.spendingLog.push({ frame: this.frame, npcNo: npc.no, description: "direction -> " + direction });
-        return direction;
-    }
-}
-
-function main() {
-    var npcs = [
+function genNpcs() {
+    return [
         nonmovableNpc(0, "短パン", [Direction.UP, Direction.RIGHT], Direction.RIGHT),
         movableNpc(1, "茶髪", 3, 2, 1, 1, Direction.LEFT),
         movableNpc(2, "警官", 2, 3, 1, 1, Direction.UP),
         movableNpc(3, "紳士", 3, 3, 1, 1, Direction.LEFT),
         nonmovableNpc(4, "婦人", [Direction.UP, Direction.RIGHT], Direction.LEFT),
     ];
+}
+
+function main2() {
+    var form = <HTMLFormElement>document.forms["f"];
+    var filterInput = <HTMLInputElement>form.elements["filter"];
+    filterInput.addEventListener("keypress", (e) => {
+        switch (e.key) {
+        case "Up":
+            filterInput.value += "↑";
+            e.preventDefault();
+            break;
+        case "Down":
+            filterInput.value += "↓";
+            e.preventDefault();
+            break;
+        case "Left":
+            filterInput.value += "←";
+            e.preventDefault();
+            break;
+        case "Right":
+            filterInput.value += "→";
+            e.preventDefault();
+            break;
+        }
+    });
+    //form.addEventListener("submit");
+}
+
+function search(seed, spendingMin, spendingMax, filter) {
+    var DIRECTION_NAME = ["↑", "↓", "←", "→"];
+    var trs = [];
+    for (var i = spendingMin; i <= spendingMax; i++) {
+        var npcs = genNpcs();
+        var simulator = new NPCSimulator(seed_step(seed, i), npcs);
+        simulator.simulate(1000);
+        var out = "";
+        simulator.actionLog.forEach(function (log) {
+            if (npcs[log.npcNo].name == "紳士") {
+                out += DIRECTION_NAME[log.direction];
+            }
+        });
+        out = out.slice(0, 20);
+        var $tr = $("<tr>");
+        $tr.append($("<th>").text(i));
+        $tr.append($("<td>").text(out));
+        trs.push($tr);
+    }
+    trs.unshift($("<tr><th>消費<th>結果</tr>"));
+    $("#result").empty().append($("<table>").append(trs));
+}
+
+function main() {
     var DIRECTION_NAME = ["上", "下", "左", "右"];
 
-    var context = new Context(0, npcs);
-    context.init();
-    context.incFrame(30);
-    for (var i = 0; i < 500; i++) {
-        context.advance();
-        context.incFrame(2);
+    var npcs = genNpcs();
+    var simulator = new NPCSimulator(0, npcs);
+    simulator.simulate(1000);
+
+    var canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var spending = new Array<number>();
+    for (var i = 0; i < simulator.frame; i++) {
+        spending[i] = 0;
     }
-    context.actionLog.forEach(function (action) {
-        console.log(action.frame + " " + npcs[action.npcNo].name + "が" + DIRECTION_NAME[action.direction] + (action.isMove ? "に移動" : "を向く"));
+    simulator.spendingLog.forEach(function (log) {
+        spending[log.frame] += 1;
+    });
+
+    var sc = 1 / 3;
+
+    npcs.forEach(function (npc, i) {
+        ctx.fillStyle = "black";
+        ctx.textBaseline = "top";
+        ctx.textAlign = "right";
+        ctx.fillText(npc.name, 50 - 5, i * 50 + 100);
+    });
+
+    ctx.translate(50, 0);
+
+    var prev = 0;
+    var sum = 0;
+    spending.forEach(function (n, i) {
+        if (n == 0) return;
+        if (i - prev >= 55) {
+            ctx.fillStyle = "#d0d0d0";
+            ctx.fillRect(prev * sc, 0, (i - prev) * sc, 500);
+            ctx.fillStyle = "black";
+            ctx.textBaseline = "top";
+            ctx.textAlign = "center";
+            ctx.fillText("消費" + sum, ((prev + i) / 2) * sc, 0);
+        }
+        prev = i;
+        sum += n;
+    });
+
+    ctx.fillStyle = "black";
+    spending.forEach(function (n, i) {
+        ctx.fillRect(i * sc, 100 - n * 20, 0.5, n * 20);
+    });
+
+
+    var colors = ["#FFAA19", "#AA6F0F", "#2A54B7", "#8E8E8E", "#FF6DB3"];
+
+    simulator.actionLog.forEach(function (log) {
+        var no = log.npcNo;
+        var npc = npcs[no];
+        var color = colors[no];
+        var frame = log.frame;
+        ctx.fillStyle = color;
+        ctx.fillRect(frame * sc, no * 50 + 100, log.isMove ? 14 * sc : 2, 50);
+        ctx.fillStyle = "black";
+        ctx.textBaseline = "top";
+        ctx.textAlign = "left";
+        ctx.fillText(DIRECTION_NAME[log.direction], frame * sc, no * 50 + 100);
     });
 }
+
+//window.onload = main; 
---
+++ npc-simulator.js
@@ -0,0 +1,212 @@
+﻿function u32(x) {
+    return x >>> 0;
+}
+
+function mul(a, b) {
+    var a1 = a >>> 16, a2 = a & 0xffff;
+    var b1 = b >>> 16, b2 = b & 0xffff;
+    return u32(((a1 * b2 + a2 * b1) << 16) + a2 * b2);
+}
+
+function seed_make_const(n) {
+    var A = 0x41c64e6d, B = 0x6073;
+    var a = A, b = B;
+    var c = 1, d = 0;
+    while (n) {
+        if (n & 1) {
+            d = u32(mul(d, a) + b);
+            c = mul(c, a);
+        }
+        b = u32(mul(b, a) + b);
+        a = mul(a, a);
+        n >>>= 1;
+    }
+    return { a: c, b: d };
+}
+
+function seed_stepper(n) {
+    var c = seed_make_const(n);
+    return function (seed) {
+        return u32(mul(seed, c.a) + c.b);
+    };
+}
+
+function seed_step(seed, n) {
+    return seed_stepper(n)(seed);
+}
+
+var seed_next = seed_stepper(1);
+
+var LCG = (function () {
+    function LCG(seed) {
+        this.seed = seed;
+    }
+    LCG.prototype.rand = function () {
+        this.seed = seed_next(this.seed);
+        return this.seed >>> 16;
+    };
+
+    LCG.prototype.step = function (n) {
+    };
+    return LCG;
+})();
+
+var NPCType;
+(function (NPCType) {
+    NPCType[NPCType["MOVABLE"] = 0] = "MOVABLE";
+    NPCType[NPCType["NONMOVABLE"] = 1] = "NONMOVABLE";
+})(NPCType || (NPCType = {}));
+
+var Direction;
+(function (Direction) {
+    Direction[Direction["UP"] = 0] = "UP";
+    Direction[Direction["DOWN"] = 1] = "DOWN";
+    Direction[Direction["LEFT"] = 2] = "LEFT";
+    Direction[Direction["RIGHT"] = 3] = "RIGHT";
+})(Direction || (Direction = {}));
+
+var DX = [0, 0, -1, 1];
+var DY = [-1, 1, 0, 0];
+
+function movable(npc) {
+    var newX = npc.x + DX[npc.direction];
+    var newY = npc.y + DY[npc.direction];
+    return 0 <= newX && newX < npc.width && 0 <= newY && newY < npc.height;
+}
+
+function move(npc) {
+    npc.x += DX[npc.direction];
+    npc.y += DY[npc.direction];
+}
+
+function movableNpc(no, name, width, height, x, y, direction) {
+    return {
+        no: no,
+        name: name,
+        type: 0 /* MOVABLE */,
+        width: width,
+        height: height,
+        allDirections: [0, 1, 2, 3],
+        x: x,
+        y: y,
+        timer: 0,
+        direction: direction,
+        state: 0
+    };
+}
+
+function nonmovableNpc(no, name, allDirections, direction) {
+    return {
+        no: no,
+        name: name,
+        type: 1 /* NONMOVABLE */,
+        width: 0,
+        height: 0,
+        allDirections: allDirections,
+        x: 0,
+        y: 0,
+        timer: 0,
+        direction: direction,
+        state: 0
+    };
+}
+
+var NPCSimulator = (function () {
+    function NPCSimulator(seed, npcs) {
+        this.lcg = new LCG(seed);
+        this.frame = 0;
+        this.npcs = npcs;
+        this.spendingLog = [];
+        this.actionLog = [];
+    }
+    NPCSimulator.prototype.simulate = function (n) {
+        this.init();
+        this.incFrame(30);
+        for (var i = 0; i < n; i++) {
+            this.advance();
+            this.incFrame(2);
+        }
+    };
+
+    NPCSimulator.prototype.init = function () {
+        var _this = this;
+        this.npcs.forEach(function (npc) {
+            if (npc.type == 1 /* NONMOVABLE */) {
+                npc.timer = _this.randTime(npc);
+            }
+        });
+    };
+
+    NPCSimulator.prototype.advance = function () {
+        var _this = this;
+        this.npcs.forEach(function (npc) {
+            _this.advanceNpc(npc);
+        });
+    };
+
+    NPCSimulator.prototype.incFrame = function (n) {
+        this.frame += n;
+    };
+
+    NPCSimulator.prototype.advanceNpc = function (npc) {
+        if (npc.type == 1 /* NONMOVABLE */) {
+            npc.timer -= 1;
+            if (npc.timer == 0) {
+                npc.timer = this.randTime(npc);
+                var direction = this.randDirection(npc);
+                if (npc.direction != direction) {
+                    this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: direction, isMove: false });
+                }
+                npc.direction = direction;
+            }
+        } else {
+            switch (npc.state) {
+                case 0:
+                    npc.state = 1;
+                    break;
+                case 1:
+                    npc.timer = this.randTime(npc) - 1;
+                    npc.state = 2;
+                    break;
+                case 2:
+                    npc.timer -= 1;
+                    if (npc.timer == 0) {
+                        var prevDirection = npc.direction;
+                        npc.direction = this.randDirection(npc);
+                        if (movable(npc)) {
+                            this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: npc.direction, isMove: true });
+                            move(npc);
+                            npc.state = 4;
+                            npc.timer = 7;
+                        } else {
+                            if (npc.direction != prevDirection) {
+                                this.actionLog.push({ frame: this.frame + 6, npcNo: npc.no, direction: npc.direction, isMove: false });
+                            }
+                            npc.state = 0;
+                        }
+                    }
+                    break;
+                case 4:
+                    npc.timer -= 1;
+                    if (npc.timer == 0) {
+                        npc.state = 0;
+                    }
+                    break;
+            }
+        }
+    };
+
+    NPCSimulator.prototype.randTime = function (npc) {
+        var time = 16 * (this.lcg.rand() % 4 + 1);
+        this.spendingLog.push({ frame: this.frame, npcNo: npc.no, description: "time -> " + time });
+        return time;
+    };
+
+    NPCSimulator.prototype.randDirection = function (npc) {
+        var direction = npc.allDirections[this.lcg.rand() % npc.allDirections.length];
+        this.spendingLog.push({ frame: this.frame, npcNo: npc.no, description: "direction -> " + direction });
+        return direction;
+    };
+    return NPCSimulator;
+})();
+//# sourceMappingURL=npc-simulator.js.map
